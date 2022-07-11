5338fad5c8f8c9b861c7e96949a19a0e20ad2766
reshad21 <miakala7@gmail.com>
2022-07-11 12:15:45 +0600

    nice work

--- README.md
+++ README.md
@@ -15,3 +15,14 @@ example: https://api.lyrics.ovh/v1/Adele/Hello
 ### main website
 
 lyrics.ovh api
+
+{data: Array(15), total: 301, next: 'http://api.deezer.com/search?limit=15&q=hi&index=15'}
+aita hoilo total data. ja akta object akare ami paici api call kore.
+
+## structure
+
+let getdata = {
+data : Array(15),
+total: 301,
+next : 'http: //api.deezer.com/search?limit=15&q=hi&index=15'
+}
--- js/main.js
+++ js/main.js
@@ -5,10 +5,25 @@ srcbtn.addEventListener('click',function(){
     let srcdata = srcinput.value;
     fetch(`https://api.lyrics.ovh/suggest/${srcdata}`)
     .then(res=>res.json())
-    .then(data=>{
-        console.log(data);
-    
+    .then(srcitem=>{
+        console.log(srcitem);
+        console.log(srcitem.data);
+        let workingdata = srcitem.data;
+        songinfo.innerHTML="";
+        for (let i = 0; i < 10; i++) {
+            const item = workingdata[i];
+            console.log(item.duration);
+            const songinfo = document.querySelector('#songinfo');
+            let songtitle = document.createElement('div');
+            songtitle.innerHTML = `<p class="api-title">${item.title} <button class="apibtn">Get Lyrics<button></p>`;
+            songinfo.appendChild(songtitle);
+        }
     });
+
+
+
+    
+    srcinput.value="";
     console.log(srcdata);
-});
+})
 
